a56ea75752e2c19d8d260074cc68601586c2d68e
John VanSchultz <johnvanschultz@upserve.com>
2018-03-13 22:01:15 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Color is strange because OpenCV us BGR for color and matplotlib uses RGB
+# Need to convert to RGB before plot with matplotlib
+
+img = cv2.imread("john.png")
+RGB_image = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+# plt.subplots(nrows=1, ncols=1), plt.imshow(RGB_image)
+# plt.show()
+
+# probably want to generate the figsize based off of pic dimensions
+plt.figure(figsize=(5, 6)), plt.imshow(RGB_image)
+# use this to turn off axis
+plt.axis('off')
+plt.show()
