b4032fa086f579b9a4468934e510b26302ace8ae
John VanSchultz <johnvanschultz@upserve.com>
2018-03-13 22:12:20 -0400

    Add uploader class

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+from uploader import ImageUploader
 
 # Color is strange because OpenCV us BGR for color and matplotlib uses RGB
 # Need to convert to RGB before plot with matplotlib
 
-img = cv2.imread("john.png")
+# img = cv2.imread("john.png")
+img = ImageUploader("john.png").img
+print(img)
 RGB_image = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 # plt.subplots(nrows=1, ncols=1), plt.imshow(RGB_image)
 # plt.show()
---
+++ uploader.py
@@ -0,0 +1,10 @@
+import cv2
+
+
+class ImageUploader:
+
+    def __init__(self, url):
+        self.img = cv2.imread("john.png")
+
+    def img(self):
+        return self.img
